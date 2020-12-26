df66aea3adf507ce59327a25c638702031b2a4b6
bibhutiregmi1 <bibhutiregmi1@gmail.com>
2020-12-26 07:33:53 +0545

    raw text detect

---
+++ main.py
@@ -0,0 +1,8 @@
+import cv2
+import pytesseract
+
+img = cv2.imread('1.png')
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+print(pytesseract.image_to_string(img))
+cv2.imshow('Result',img)
+cv2.waitKey(0)
